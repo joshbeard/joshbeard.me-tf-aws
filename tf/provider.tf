b8f36071a397781fb9d0f26a9ba173e409afed4c
Josh Beard <josh@joshbeard.me>
2022-03-11 13:03:03 -0700

    Remove obsoleted code

--- tf/provider.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region  = "us-west-2"
-}
-
-# Certificates use the 'us-east-1' region.
-provider "aws" {
-  region  = "us-east-1"
-  alias   = "cert"
-}
--- tf/tfstate.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "tf-remote-state20201021064517629300000002"
-    key     = "joshbeard.me/terraform.tfstate"
-    region  = "us-west-2"
-    encrypt = true
-    kms_key_id = "04dc01e6-bd7b-4873-8468-68ba0217a4e2"
-  }
-}
